d3dbb396402827241b9a527fdefa6fe2d90f5624
Qazeer <qazeer@protonmail.com>
2023-02-22 21:35:32 +0100

    Remove headers from Python helper script

--- Helpers/Generate_fields_mapping_for_CSV.py
+++ Helpers/Generate_fields_mapping_for_CSV.py
@@ -1,4 +1,4 @@
-fields_csv_header = 'SourceFile,SourceCreated,SourceModified,SourceAccessed,AppId,AppIdDescription,EntryName,TargetCreated,TargetModified,TargetAccessed,FileSize,RelativePath,WorkingDirectory,FileAttributes,HeaderFlags,DriveType,VolumeSerialNumber,VolumeLabel,LocalPath,CommonPath,TargetIDAbsolutePath,TargetMFTEntryNumber,TargetMFTSequenceNumber,MachineID,MachineMACAddress,TrackerCreatedOn,ExtraBlocksPresent,Arguments'
+fields_csv_header = ''
 
 i = 0
 for field in fields_csv_header.split(','):
