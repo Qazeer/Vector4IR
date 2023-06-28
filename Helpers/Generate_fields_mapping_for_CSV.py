b8111c0b9f16006161f1e538a2b25a2a5e4e2ca9
Qazeer <qazeer@protonmail.com>
2023-06-29 00:49:02 +0200

    Update generate fields mapping script

--- Helpers/Generate_fields_mapping_for_CSV.py
+++ Helpers/Generate_fields_mapping_for_CSV.py
@@ -2,5 +2,6 @@ fields_csv_header = ''
 
 i = 0
 for field in fields_csv_header.split(','):
-    print(f'.{field.lower()} = fields[{i}]')
+    field_key = field.lower().replace(' ', '').replace('"', '').replace('/', '_')
+    print(f'.{field_key} = fields[{i}]')
     i = i + 1
